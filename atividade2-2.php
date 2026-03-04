de00aa028ee849ee833eb39b44b971b81699bf16
LuisMiguelDaSilvaPacheco <lm.luismiguel10@gmail.com>
2026-03-04 13:49:12 -0300

    Fix loop condition to continue while value is positive

--- atividade2-2.php
+++ atividade2-2.php
@@ -6,6 +6,6 @@ $valorArrecadado = 0;
 do {
     $valor = readline("Digite um valor para ajudar, 0 para cancelar: ");
     $valorArrecadado += $valor;
-} while ($valor != 0);
+} while ($valor > 0);
 
 print "O valor total arrecadado foi R$$valorArrecadado,00 \n";
